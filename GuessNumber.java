8956a1ef81d9bd36f6a9e798dee9911f800c1a75
João Carlos Pontes <45675983+jcarlospontes@users.noreply.github.com>
2022-10-01 20:33:41 -0300

    switch case option

--- GuessNumber.java
+++ GuessNumber.java
@@ -17,13 +17,16 @@ public class Solution extends GuessGame {
         while(low <= high) {
             mid = low+(high-low)/2;
             res = guess(mid); 
-            if(res == 0)
-                return mid;
-            else if(res == -1) {
-                high = mid-1;
+            switch(res){
+                case 0:
+                    return mid;
+                case -1:
+                    high = mid-1;
+                    break;
+                case 1:
+                    low = mid+1;
+                    break;
             }
-            else if(res == 1)
-                low = mid+1;
         }
         
         return 0;
