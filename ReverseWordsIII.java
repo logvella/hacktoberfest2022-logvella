b7e2ee3bebd6b44e3576326b4617fbdbfd37da56
power-coder-java <114444122+power-coder-java@users.noreply.github.com>
2022-10-02 01:02:24 +0530

    Create ReverseWordsIII.java

---
+++ ReverseWordsIII.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder output = new StringBuilder();
+        String[] arr = s.split(" ");
+        StringBuilder rev = new StringBuilder();
+        for(String str : arr) {
+            output.append(rev.append(str).reverse().toString()+" ");
+            rev.setLength(0);
+        }
+        
+        return output.toString().trim();
+    }
+}
