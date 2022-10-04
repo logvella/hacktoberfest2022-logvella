3553ba617e32bbc03d5832967c2e8d03c80e6fc3
power-coder-java <114444122+power-coder-java@users.noreply.github.com>
2022-10-05 00:36:20 +0530

    Create FibonacciNormal.java

---
+++ FibonacciNormal.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class fibonacci {
+    
+    public static int fib(int n) {
+        if(n<=1)
+            return n;
+        return fib(n-1)+fib(n-2);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(fib((new Scanner(System.in)).nextInt()));
+    }
+}
